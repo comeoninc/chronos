39c15660c1501839343b0784d7526ffcfb44e60d
C'mon Inc <43278368+comeoninc@users.noreply.github.com>
2019-07-25 19:57:27 +0700

    helmet added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as helmet from 'helmet';
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
@@ -7,11 +9,14 @@ import {
 import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create<NestFastifyApplication>(
     ApplicationModule,
     new FastifyAdapter(),
   );
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(new ValidationPipe, helmet ());
   await app.listen(3000);
+
 }
 bootstrap();
